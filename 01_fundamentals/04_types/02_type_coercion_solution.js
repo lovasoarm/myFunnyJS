17e68e7248596cb97e0c89faa36bc43558e2a90f
Lovasoa RM <74265870+lovasoarm@users.noreply.github.com>
2026-03-31 10:51:48 +0300

    Fix type coercion examples and comments

--- 01_fundamentals/04_types/02_type_coercion_solution.js
+++ 01_fundamentals/04_types/02_type_coercion_solution.js
@@ -1,13 +1,13 @@
-console.log("10" + 5);
-console.log("10" - 5); //forcing calcul
-console.log(true + 1); //true s'évalue à 1
-console.log(false + 1); //false s'évalue en 0
+console.log("10" + 5); //105
+console.log("10" - 5); //forcing calcul -> 5
+console.log(true + 1); //true s'évalue à 1 donc 1+1 = 2
+console.log(false + 1); //false s'évalue en 0 donc 1+0 = 1
 
 console.log(null == 0); // false ? null est seulement égal à undefined en comparaison lâche. Mais null ne devient PAS 0.
-console.log(null == undefined);
-console.log(null === undefined); //pas le meme type
+console.log(null == undefined); //true
+console.log(null === undefined); //pas le meme type -> false
 
 let input = "0";
 if (input) {
-  console.log("quelquechose");
+  console.log("quelquechose"); //true -> string non-vide
 }
