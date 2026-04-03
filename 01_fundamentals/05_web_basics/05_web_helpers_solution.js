63cf39d65f88bbd17d847477fc7f5f877c97385c
Lovasoa RM <74265870+lovasoarm@users.noreply.github.com>
2026-04-03 11:34:41 +0300

    Update 05_web_helpers_solution.js

--- 01_fundamentals/05_web_basics/05_web_helpers_solution.js
+++ 01_fundamentals/05_web_basics/05_web_helpers_solution.js
@@ -13,7 +13,9 @@ function on(cible, event, callback) {
 function remove(element) {
   element.remove();
 }
-//MISSION 1 — LE BOUTON CHAOS
+
+
+//MISSION 1 : LE BOUTON CHAOS -> Crée un bouton "Spawn Zombie". Quand on clique, un nouveau zombie apparaît dans la page.
 let button = create("button");
 addTitle(button, "Spawn Zombie");
 append(document.body, button);
@@ -23,7 +25,10 @@ on(button, "click", () => {
   addTitle(p, "🧟 un zombie apparaît !");
   append(document.body, p);
 });
-//MISSION 2 — L’ARMÉE DES MONSTRES
+
+
+
+//MISSION 2 : L’ARMÉE DES MONSTRES -> Crée un bouton "Spawn 5 monsters". Quand on clique, 5 monstres apparaissent. Utilise une boucle.
 let button2 = create("button");
 addTitle(button2, "Spawn 5 monsters");
 append(document.body, button2);
@@ -32,11 +37,14 @@ on(button2, "click", () => {
   for (let i = 0; i < 5; i++) {
     // boucle à l'intérieur du clic
     let p = create("p");
-    addTitle(p, "🧟 un zombie apparaît !");
+    addTitle(p, "grrr un zombie apparaît !");
     append(document.body, p);
   }
 });
-//MISSION 3 — LE DESTRUCTEUR
+
+
+
+//MISSION 3 : LE DESTRUCTEUR -> Chaque monstre doit avoir un bouton "Kill". Quand on clique dessus, le monstre disparaît.
 let button3 = create("button");
 addTitle(button3, "Spawn monsters with kill");
 append(document.body, button3);
@@ -57,7 +65,10 @@ on(button3, "click", () => {
     });
   }
 });
-//MISSION 4 — LE SPAWNER ALÉATOIRE
+
+
+
+//MISSION 4 : LE SPAWNER ALÉATOIRE -> À chaque clic, un monstre aléatoire du tableau apparaît.
 let monsters = ["Goblin", "Slime", "Dragon", "Ghost"];
 
 function randomItem(tab) {
@@ -72,10 +83,13 @@ on(button4, "click", () => {
   addTitle(p, randomItem(monsters));
   append(document.body, p);
 });
-//MISSION 5 — LE HELPER SUPRÊME
+
+
+
+//MISSION 5 : LE HELPER SUPRÊME -> Crée une fonction spawnMonster(name) qui crée un élément, lui donne un texte, et l'ajoute dans la page. Utilise-la pour spawner 10 monstres différents.
 function spawnMonster(name) {
   let elt = create("p");
-  addTitle(elt, `🧟 ${name}`);
+  addTitle(elt, `grrrr ${name}`);
   append(document.body, elt);
 }
 const allMonsters = [
