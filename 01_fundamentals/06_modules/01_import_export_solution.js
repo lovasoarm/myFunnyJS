ea46d73e41bfbf5dec9252cec8b0d5d1a95ba8ad
Lovasoa RM <74265870+lovasoarm@users.noreply.github.com>
2026-04-30 13:13:10 +0300

    Refactor import/export structure for ninja filtering

--- 01_fundamentals/06_modules/01_import_export_solution.js
+++ 01_fundamentals/06_modules/01_import_export_solution.js
@@ -6,6 +6,7 @@ filtres.js : exporte deux fonctions : une pour filtrer par rang, une pour filtre
 main.js : importe tout, affiche les Jonin du clan Uchiha
 Contrainte : main.js ne contient aucune logique de filtrage. Tout ça vit dans filtres.js.
 */
+
 // Dans listes.js : 
 const listes = [
   { nom: "Kakashi", rang: "Jonin", clan: "Hatake" },
@@ -13,10 +14,12 @@ const listes = [
   { nom: "Itachi", rang: "Jonin", clan: "Uchiha" }
 ]
 export default listes //toujours à la fin
-// dans filtres.js
+
+// Dans filtres.js
 //faut comparer avec la valeur que tu veux
 export const filteredByRange = (ninjas, rangCible) => ninjas.filter((n) => n.rang === rangCible)
 export const filteredByClan = (ninjas, clanCible) => ninjas.filter((n) => n.clan === clanCible)
+
 //Dans main.js
 import listes from "./ninjas.js"
 import { filteredByRange, filteredByClan } from "./filtres.js"
