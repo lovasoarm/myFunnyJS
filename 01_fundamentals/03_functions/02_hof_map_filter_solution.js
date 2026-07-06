655838bf03b9c8767fecc981908e556af3325c98
lovasoarm <lovasoarm@gmail.com>
2026-07-06 13:51:43 +0300

    [fix bug in v4]

--- 01_fundamentals/03_functions/02_hof_map_filter_solution.js
+++
@@ -1,9 +0,0 @@
-let players = [
-  { name: "boby", hp: 100 },
-  { name: "elonmusk", hp: 200 },
-  { name: "ronaldo", hp: 10 },
-];
-let hpSquared = players.map((player) => player.hp * 2);
-let hpFiltered = players.filter((player) => player.hp > 100);
-console.log(hpSquared);
-console.log(hpFiltered);
